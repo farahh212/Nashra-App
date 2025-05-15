06a3b659d0dfaf96e30f28bd537caa3de92d2131
Cisco <manoshalaa@gmail.com>
2025-05-15 22:28:10 +0300

    save my changes

---
+++ lib/l10n/app_localizations.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class AppLocalizationsSetup {
+  static const Iterable<LocalizationsDelegate<dynamic>> localizationsDelegates = [
+    AppLocalizations.delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ];
+
+  static const Iterable<Locale> supportedLocales = [
+    Locale('en'), // English
+    Locale('ar'), // Arabic
+  ];
+} 
