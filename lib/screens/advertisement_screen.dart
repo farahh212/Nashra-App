a089338266aa30e2b8169f31e9e4bccaa2c82d81
hagaradel12 <hagaradel198@gmail.com>
2025-05-15 22:02:53 +0300

    Advertisments 1at part

--- lib/screens/advertisement_screen.dart
+++ lib/screens/advertisement_screen.dart
@@ -3,6 +3,7 @@ import 'package:nashra_project2/models/index.dart';
 import 'package:nashra_project2/providers/advertisementProvider.dart';
 import 'package:nashra_project2/providers/authProvider.dart';
 import 'package:nashra_project2/screens/ad_card.dart';
+import 'package:nashra_project2/screens/create_ad_screen.dart';
 import 'package:provider/provider.dart';
 
 class AdvertisementScreen extends StatefulWidget {
@@ -50,11 +51,30 @@ class _AdvertisementScreenState extends State<AdvertisementScreen> {
                   itemBuilder: (context, index) => AdCard(ad: ads[index]),
                 ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Navigate to ad submission form
-        },
-        child: Icon(Icons.add),
+  onPressed: () {
+    // Navigate to the advertisement creation screen
+    Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => CreateAdScreen()));
+  },
+  backgroundColor: Colors.transparent, // Make background transparent
+  elevation: 0, // Remove shadow
+  child: Container(
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      border: Border.all(
+        color: Colors.green.shade800, // Border color
+        width: 3.0, // Border width
       ),
+    ),
+    padding: EdgeInsets.all(10),
+    child: Icon(
+      Icons.add,
+      color: Colors.green.shade800,
+      size: 30,
+    ),
+  ),
+),
+
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
---
+++ lib/screens/gov_advertisments.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class governemntAdvertismentsScreen extends StatefulWidget {
+  const governemntAdvertismentsScreen({super.key});
+  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+
+}
